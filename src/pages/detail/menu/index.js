2373fcdd48cb09527ffbbdeee5d394a750d13568
Kirito-vr <3470728937@qq.com>
2022-08-03 21:15:21 +0800

    优化商城界面

---
+++ src/pages/detail/menu/index.js
@@ -0,0 +1,14 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import classNames from 'classnames'
+import './index.scss'
+
+export default class Menu extends Component{
+  render(){
+    return(
+      <View>
+        
+      </View>
+    )
+  }
+}
