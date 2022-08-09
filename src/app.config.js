1f3c383c7b59c295cd7bd6b14da914bece76246e
xiaoming <bv87ar@163.com>
2022-08-09 14:54:31 +0800

    首页修改

--- src/app.config.js
+++ src/app.config.js
@@ -4,6 +4,7 @@ export default defineAppConfig({
     'pages/user/index',
     'pages/cart/index',
     'pages/detail/index',
+    'pages/test/test',
   ],
   window: {
     backgroundTextStyle: 'light',
---
+++ src/pages/test/test.tsx
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import {View} from "@tarojs/components";
+
+
+export default class Test extends Component{
+  constructor(props){
+    super(props)
+  }
+    render(){
+      return (
+          <View>跳转测试</View>
+      );
+    }
+}
